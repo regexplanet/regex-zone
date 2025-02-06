073245fb3374595bc2a872b715cc5ebf1a8c19f2
Andrew Marcuse <fileformat@gmail.com>
2025-02-06 18:52:13 -0500

    Add links to patterns page

--- README.md
+++ README.md
@@ -41,8 +41,11 @@ LATER
 
 ## Credits
 
+[![Bootstrap](https://www.vectorlogo.zone/logos/getbootstrap/getbootstrap-ar21.svg)](https://getbootstrap.com/ "HTML/CSS Framework")
 [![Google CloudRun](https://www.vectorlogo.zone/logos/google_cloud_run/google_cloud_run-ar21.svg)](https://cloud.google.com/run/ "Hosting")
+[![Digital Ocean](https://www.vectorlogo.zone/logos/digitalocean/digitalocean-ar21.svg)](https://m.do.co/c/976f479b2317 "Hosting")
 [![Docker](https://www.vectorlogo.zone/logos/docker/docker-ar21.svg)](https://www.docker.com/ "Deployment")
+[![Drizzle ORM](https://www.vectorlogo.zone/logos/drizzleteam/drizzleteam-ar21.svg)](https://orm.drizzle.team/ "ORM")
 [![Git](https://www.vectorlogo.zone/logos/git-scm/git-scm-ar21.svg)](https://git-scm.com/ "Version control")
 [![Github](https://www.vectorlogo.zone/logos/github/github-ar21.svg)](https://github.com/ "Code hosting")
 [![Google Noto Emoji](https://www.vectorlogo.zone/logos/google/google-ar21.svg)](https://github.com/googlefonts/noto-emoji/blob/master/svg/emoji_u1f441.svg "Logo/Favicon")
@@ -50,11 +53,11 @@ LATER
 [![NodePing](https://www.vectorlogo.zone/logos/nodeping/nodeping-ar21.svg)](https://nodeping.com?rid=201109281250J5K3P "Uptime monitoring")
 [![npm](https://www.vectorlogo.zone/logos/npmjs/npmjs-ar21.svg)](https://www.npmjs.com/ "JS Package Management")
 [![Phosphor Icons](https://www.vectorlogo.zone/logos/phosphoricons/phosphoricons-ar21.svg)](https://phosphoricons.com/ "Toolbar icons")
+[![PostgreSQL](https://www.vectorlogo.zone/logos/postgresql/postgresql-ar21.svg)](https://www.postgresql.org/ "Database")
+[![Postgres.js](https://www.vectorlogo.zone/logos/github_postgresjs/github_postgresjs-ar21.svg)](https://github.com/porsager/postgres "Database driver")
 [![react.js](https://www.vectorlogo.zone/logos/reactjs/reactjs-ar21.svg)](https://reactjs.org/ "UI Framework")
 [![Remix](https://www.vectorlogo.zone/logos/remixrun/remixrun-ar21.svg)](https://remix.run/ "React Framework")
+[![SVGR](https://www.vectorlogo.zone/logos/react-svgr/react-svgr-ar21.svg)](https://react-svgr.com/ "SVG conversion")
 [![TypeScript](https://www.vectorlogo.zone/logos/typescriptlang/typescriptlang-ar21.svg)](https://www.typescriptlang.org/ "Programming Language")
 [![Vite](https://www.vectorlogo.zone/logos/vitejsdev/vitejsdev-ar21.svg)](https://vitejs.dev/ "Bundler")
 
-* [SVGR](https://react-svgr.com/)
-* [Drizzle ORM](https://orm.drizzle.team/)
-* [Postgres.js](https://github.com/porsager/postgres)
--- app/routes/patterns._index.tsx
+++ app/routes/patterns._index.tsx
@@ -1,59 +1,79 @@
 import type { MetaFunction } from "@remix-run/node";
-import { Link as RemixLink, useLoaderData } from "@remix-run/react";
+import { Link as RemixLink, useLoaderData, useRouteLoaderData } from "@remix-run/react";
 import { json } from "@remix-run/node";
+import { desc, arrayContains } from "drizzle-orm";
 
 import { getAll, initialize, PatternEntry } from "~/components/Patterns";
 import { TagList } from "~/components/TagList";
 import { PatternTagUrlBuilder } from "~/util/PatternTagUrlBuilder";
+import LinksTable from "~/components/LinksTable";
+import { RootLoaderData } from "~/types/RootLoaderData";
+import { dborm } from "~/db/connection.server";
+import { regex_link } from "~/db/schema";
 
 export const loader = async () => {
-    await initialize();
-    return json(getAll());
+	await initialize();
+
+	const links = await dborm.select()
+		.from(regex_link)
+		.where(arrayContains(regex_link.rxl_tags, ["patterns"]))
+		.orderBy(desc(regex_link.rxl_created_at))
+		.limit(100);
+
+	return json({
+		patterns: getAll(),
+		links,
+	});
 };
 
 export const meta: MetaFunction = () => {
-    return [
-        { title: "Patterns - Regex Zone" },
-        { name: "description", content: "A collection of useful regular expression patterns" },
-    ];
+	return [
+		{ title: "Patterns - Regex Zone" },
+		{ name: "description", content: "A collection of useful regular expression patterns" },
+	];
 };
 
 function PatternEntryRow(entry: PatternEntry) {
-    return (
-        <tr key={entry.handle}>
-            <td>
-                <RemixLink to={`${entry.handle}/`}>{entry.title}</RemixLink>
-            </td>
-            <td style={{ 'textAlign': 'right' }}>
-                {entry.tags ? <TagList tags={entry.tags} urlBuilder={PatternTagUrlBuilder} /> : null}
-                <div className="badge text-bg-secondary">{entry.variations.length}</div>
-            </td>
-        </tr>
-    );
+	return (
+		<tr key={entry.handle}>
+			<td>
+				<RemixLink to={`${entry.handle}/`}>{entry.title}</RemixLink>
+			</td>
+			<td style={{ 'textAlign': 'right' }}>
+				{entry.tags ? <TagList tags={entry.tags} urlBuilder={PatternTagUrlBuilder} /> : null}
+				<div className="badge text-bg-secondary">{entry.variations.length}</div>
+			</td>
+		</tr>
+	);
 }
 
 export default function Index() {
-    const entries = useLoaderData<typeof loader>();
+	const { user } = useRouteLoaderData<RootLoaderData>("root") as unknown as RootLoaderData;
+	const data = useLoaderData<typeof loader>();
+	const links = data.links as unknown as typeof regex_link.$inferSelect[];
 
-    const entryRows = entries.map((entry) => PatternEntryRow(entry));
+	const entryRows = data.patterns.map((entry) => PatternEntryRow(entry));
 
-    return (
-        <>
-            <h1 className="py-2">Patterns</h1>
-            <table className="table table-striped table-hover">
-                <thead>
-                    <tr>
-                        <th>Title</th>
-                        <th>&nbsp;</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {entryRows}
-                </tbody>
-            </table>
-            <details><summary>Raw data</summary>
-                <pre>{JSON.stringify(entries, null, 4)}</pre>
-            </details>
-        </>
-    );
+	return (
+		<>
+			<h1 className="py-2">Patterns</h1>
+			<table className="table table-striped table-hover">
+				<thead>
+					<tr>
+						<th>Title</th>
+						<th>&nbsp;</th>
+					</tr>
+				</thead>
+				<tbody>
+					{entryRows}
+				</tbody>
+			</table>
+			<h2 className="pt-3">Links</h2>
+			<div className="alert alert-info">Other websites with useful regex patterns</div>
+			<LinksTable currentUrl="/links/" links={links} isAdmin={user?.isAdmin} />
+			<details><summary>Raw data</summary>
+				<pre>{JSON.stringify(data.patterns, null, 4)}</pre>
+			</details>
+		</>
+	);
 }
